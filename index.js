cc90ac9fa790dd995de583844285fe793d562deb
Abby Lortz <abilenelortz03@gmail.com>
2018-06-20 21:18:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge)
 const randomNumber = Math.floor(Math.random() * 20) + 0;
 const randomInteger = Math.floor(randomNumber);
-const randomUserID = Math.floor
+const randomUserID = Math.floor(Math.random() * 1000000001) + 10000000020;
